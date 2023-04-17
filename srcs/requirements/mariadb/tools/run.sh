f857e5f0a574fd295a3c7816e90404a9350895ad
Samuel Janssens <sajansse@c2r9p2.42nice.fr>
2023-04-17 23:07:10 +0200

    oe

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -1,38 +1,52 @@
 #!/bin/bash
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "Configuration de la base de donnée..."
+    echo "[SETUP] Configuration de la base de donnée..."
     mysql_install_db \
     --user=mysql \
     --datadir=/var/lib/mysql  \
     --port=3306 \
     --socket=/run/mysqld/mysqld.sock \
-    --bind_address=* > /dev/null
+    --bind_address=* > /dev/null 2>&1
 
+    echo "[SETUP] Démarrage de mysql..."
+    service mysql start
 
-    service mysql start > /dev/null 2>&1
     # Creation de la table
+    echo "[SETUP] Creation de la base donnée..."
     mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+    sleep 1
 
     # Creation de l'utilisateur
+    echo "[SETUP] Creation de l'utilisateur..."
     mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+    sleep 1
 
     # Donner tous les droit a un user
-    mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+    echo "[SETUP] Modification des privilèges..."
+    mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+    sleep 1
 
     # Modification de l'utilisateur root
-    # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+    echo "[SETUP] Modification du mot de passe root..."
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+    sleep 1
 
-    # # On actualise les privilèges
-    # mysql -e "FLUSH PRIVILEGES;"
+    # On actualise les privilèges
+    echo "[SETUP] Actualisation des privilèges..."
+    mysql -e "FLUSH PRIVILEGES;" -u root -p"$SQL_ROOT_PASSWORD"
+    sleep 1
 
-    # # Redemarrage de mysql
-    # mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-    echo "Configuration de la base de donnée terminée."
+    # Redemarrage de mysql
+    echo "[SETUP] Redemarrage de mysql..."
+    mysqladmin --user=root --password=$SQL_ROOT_PASSWORD shutdown
+
+    echo "[SETUP] Configuration de la base de donnée terminée."
 else
-    echo "Base de donnée déjà existante."
+    echo "[SETUP] Base de donnée déjà existante."
 fi
 
+sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
 
 # Execution recommandé de mysql
-#exec mysqld_safe > /dev/null 2>&1
-exec mysqld_safe 
+echo "---Démarrage du service MariaDB---"
+exec mysqld_safe
--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -5,36 +5,45 @@
 
 # On attend que la databse est bien demarrée
 
+START=$(date +%s)
 #On télécharge Wordpress
 if [ -f "/var/www/wordpress/index.php" ]; then
-	echo "Installation de Wordpress déjà existante."
+	echo "[INSTALL] Installation de Wordpress déjà existante."
 else
-	echo "Installation de Wordpress..."
+	echo "[INSTALL] Téléchargement de Wordpress..."
 	wget https://fr.wordpress.org/wordpress-6.1.1-fr_FR.tar.gz -P /var/www > /dev/null 2>&1 
+
 	# On le décompresse et supprime le zip
+	echo "[INSTALL] Décompression de l'archive..."
 	cd /var/www && tar -xzf wordpress-6.1.1-fr_FR.tar.gz && rm wordpress-6.1.1-fr_FR.tar.gz > /dev/null 2>&1
+
 	# On ajoute les droits
+	echo "[INSTALL] Modification des droits d'accès..."
 	chown -R root:root /var/www/wordpress
-	echo "Installation de Wordpress terminée."
+
+	echo "[INSTALL] Installation de Wordpress terminée."
+
 fi
 
-sleep 10
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+
+if [ "$DIFF" -lt 10 ]; then
+	sleep $((10 - $DIFF))
+fi
 
 # Pour eviter une eventuelle erreur de PHP
 if [ ! -d "/run/php" ]; then
 	mkdir /run/php
 fi
 
-cd /var/www/wordpress
 # Pour eviter de reconfigurer wordpress a chaque fois
 FILE=/var/www/wordpress/wp-config.php
 if [ ! -f "$FILE" ]; then
-	echo "Configuration de wordpress..."
+	echo "[SETUP] Configuration de Wordpress..."
 	mv /tmp/wp-config.php /var/www/wordpress/wp-config.php
-	echo "Configuration de wordpress terminée."
 fi
 
 # Demarrage de PHP
-echo "Démarrage de PHP..."
-/usr/sbin/php-fpm7.3 -F > /dev/null 2>&1 &
-echo "PHP démarré."
+echo "---Démarrage du service Wordpress.---"
+/usr/sbin/php-fpm7.3 -F
